bc896dd677cbdca9b8b0e95de3cbf65b6e2fec23
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-11-12 16:53:22 +0100

    ade it

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |i|
+  Article.create(title: Faker::Team.creature, content: Faker::Lorem.sentence(word_count: 25))
+end
